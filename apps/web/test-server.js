71536cbd095cf64e6c3b68ed8c9b41c84aa25789
othesisrentals <othesisrentals@gmail.com>
2026-06-30 16:52:11 +0100

    Switch to Next.js for Vercel deployment

--- apps/web/test-server.js
+++
@@ -1,8 +0,0 @@
-import { createServer } from 'node:http';
-import handler from './api/index.js';
-
-const server = createServer(handler);
-
-server.listen(3000, () => {
-  console.log('Test server running at http://localhost:3000');
-});
